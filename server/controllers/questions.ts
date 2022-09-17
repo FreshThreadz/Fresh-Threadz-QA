51756d0740d0835ae43e34df83bfdfdadd897869
allanviguilla <viguilla.allan@gmail.com>
2022-09-17 12:01:39 -0700

    Fix queries and get correctly shaped data

--- server/controllers/questions.ts
+++ server/controllers/questions.ts
@@ -5,7 +5,16 @@ const models = require('../models/questions');
 // PUT /qa/questions/:question_id/helpful
 module.exports = {
   getQuestions: async (req, res) => {
+    console.log('req.query in controller....', req.query);
+    console.log('req.params in controller....', req.params);
+    // console.log('res...', res);
     const fetchQuestions = await models.getAll();
+    const questionsResponse = {
+      "product_id": req.query.product_id,
+      "results": fetchQuestions.rows,
+    };
+    res.send(questionsResponse);
+    // console.log(questionsReponse);
     return res.json(fetchQuestions);
   },
   postQuestion: async (req, res) => {
--- server/models/questions.js
+++ server/models/questions.js
@@ -2,41 +2,15 @@
 const { pool } = require('../db/postgres');
 module.exports = {
     getAll: async () => {
-        const queryStr = `SELECT
-      json_build_object(
-        'product_id', questions.product_id,
-        'results', json_build_array(
-          SELECT json_build_object(
-            'question_id', questions.id,
-            'question_body', questions.body,
-            'asker_name', questions.asker_name,
-            'question_helpfulness', questions.helpful,
-            'reported', questions.reported,
-            'answers', json_build_object(
-              answers.id, json_build_object(
-                'id', answers.id,
-                'body', answers.body,
-                'date', answers.date_written,
-                'answerer_name', answers.answerer_name,
-                'helpfulness', answers.helpful,
-                'photos', json_build_array(
-                  answers_photos
-                )
-              )
-            )
-          )
-        )
-      )
-      FROM questions
-      INNER JOIN answers ON id = question_id`;
+        const queryStr = `SELECT id, product_id, body, date_written, asker_name, asker_email, reported, helpful, (SELECT id, body, date_written, answerer_name, helpful FROM answers a WHERE a.id = q.id) answers FROM questions q`;
         const fetchQuestions = await pool.query(queryStr);
-        console.log(fetchQuestions);
+        // console.log(fetchQuestions);
         return fetchQuestions;
     },
     post: async (params) => {
         const queryStr = "INSERT INTO questions (body, asker_name, asker_email, product_id) VALUES ($1, $2, $3, $4)";
         const postQuestion = await pool.query(queryStr, params);
-        console.log(postQuestion);
+        // console.log(postQuestion);
         return postQuestion;
     }
 };
