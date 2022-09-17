7ac0391596e9a14f99ac9703e5f9a1cd1e8d3152
allanviguilla <viguilla.allan@gmail.com>
2022-09-17 10:47:05 -0700

    Attempt nested queries to mimic API data shape

--- server/models/questions.js
+++ server/models/questions.js
@@ -2,7 +2,33 @@
 const { pool } = require('../db/postgres');
 module.exports = {
     getAll: async () => {
-        const queryStr = "SELECT * FROM questions LIMIT 5";
+        const queryStr = `SELECT
+      json_build_object(
+        'product_id', questions.product_id,
+        'results', json_build_array(
+          SELECT json_build_object(
+            'question_id', questions.id,
+            'question_body', questions.body,
+            'asker_name', questions.asker_name,
+            'question_helpfulness', questions.helpful,
+            'reported', questions.reported,
+            'answers', json_build_object(
+              answers.id, json_build_object(
+                'id', answers.id,
+                'body', answers.body,
+                'date', answers.date_written,
+                'answerer_name', answers.answerer_name,
+                'helpfulness', answers.helpful,
+                'photos', json_build_array(
+                  answers_photos
+                )
+              )
+            )
+          )
+        )
+      )
+      FROM questions
+      INNER JOIN answers ON id = question_id`;
         const fetchQuestions = await pool.query(queryStr);
         console.log(fetchQuestions);
         return fetchQuestions;
